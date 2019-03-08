e5fa51dee826bc4f809898767cab9de85b43196c
Amod Panchal <32383876+amod26@users.noreply.github.com>
2019-03-08 13:52:41 -0500

    Creating Sample GUI box with Tkinter

---
+++ GUI intro.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+win=tk.Tk() # create an instance
+
+win.title("Python GUI")
+
+# keeping default win size 
+win.minsize(300,1) 
+
+# disable resizing window
+# win.resizable(0, 0) 
+
+win.mainloop() # will keep the window open
