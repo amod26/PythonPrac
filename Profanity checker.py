1e0244dddf668af58188a5cbbef75b02cb81340d
Amod Panchal <32383876+amod26@users.noreply.github.com>
2019-04-18 11:20:17 -0400

    Checks if the document contains profanity words

---
+++ Profanity checker.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+import requests
+
+def profanity():
+    file_obj = open('/Users/senorpete/Desktop/Pirate.txt', 'r')
+    content = file_obj.read()
+    # print content
+    file_obj.close()
+    check_profanity(content)
+
+def check_profanity(text):
+    obj = requests.get('http://www.wdylike.appspot.com/?q=' + str(text))
+    if 'true' in obj.text:
+        print ("Profanity alert! Check your document")
+    elif 'false' in obj.text:
+        print ("No curse words found")
+    else:
+        print ("Unable to scan the document")
+
+
+if __name__ == '__main__':
+    profanity()
+
+
+# In[ ]:
+
+
+
+
