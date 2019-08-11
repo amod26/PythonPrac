ea65069231e8f2061d105f766b7e04367b4e576c
Amod Panchal <32383876+amod26@users.noreply.github.com>
2019-08-11 19:20:04 -0400

    Automatically Moves and clicks mouse

---
+++ Auto Mouse mover & clicker.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# pip install pyautogui
+
+
+# In[2]:
+
+
+import pyautogui
+
+
+# In[3]:
+
+
+print(pyautogui.size()) # prints the size of the screen 
+
+
+# In[4]:
+
+
+pyautogui.moveTo(150,100,duration=2) # moves mouse position from the given coordinates
+
+
+# In[5]:
+
+
+pyautogui.click(clicks=2) # clicks twice
+
+
+# In[ ]:
+
+
+
+
